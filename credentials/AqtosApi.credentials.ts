13c8bb18c472b996343008029162d5fc14c1732b
Antonij Simonovski <antonijsimonovski@gmail.com>
2025-12-23 13:38:23 +0100

    added credentials js file

--- credentials/AqtosApi.credentials.ts
+++ credentials/AqtosApi.credentials.ts
@@ -10,7 +10,7 @@ export class AqtosApi implements ICredentialType {
 
   displayName = "Aqtos API";
 
-  icon: Icon = "file:../icons/aqtos.svg"
+  icon: Icon = "file:../icons/aqtos.svg";
 
   documentationUrl =
     "https://www.npmjs.com/package/n8n-nodes-aqtos#credentials";
